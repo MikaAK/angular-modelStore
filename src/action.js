9d6260736028ff5a6a64b1d459c6e7ffb0760ab7
MikaAK <mikakalathil@gmail.com>
2015-03-12 14:41:10 -0700

    Fix some action binding

--- src/action.js
+++ src/action.js
@@ -2,27 +2,46 @@
 
 angular.module('not-flux')
   .service('Action', ['$rootScope', ($rootScope) => {
+    /**
+     * Representation of a action that we use to trigger store events
+     * @class
+     * @param {string} actionName - The action name
+     * @returns An callback representing that action
+     **/
     return class Action {
+      /**
+       * Create a bunch of new actions usign an array
+       * @func
+       * @param {array} actionList - An array of actionNames to create
+       * @returns A object with actions
+       **/
       static createFromList(actionList) {
         if (!angular.isArray(actionList)) throw new Error('You must pass actions an array')
 
         let functionSet = {}
 
         actionList.forEach(action => {
-          var newAction = new Action(action)
-
-          functionSet[action]          = newAction.callAction.bind(newAction)
-          functionSet[action].listenTo = newAction.listenTo.bind(newAction)
+          functionSet[action] = new Action(action)
         })
 
         return functionSet
       }
 
       constructor(actionName) {
+        var newAction   = this.callAction.bind(this)
         this.name       = actionName
         this._listeners = []
+
+        newAction.listenTo = this.listenTo.bind(this)
+
+        return newAction
       }
 
+      /**
+       * Add a callback as a listener to an action
+       * @func
+       * @param {function} callback - A single callback or an array of callbacks
+       **/
       listenTo(callback) {
         this._listeners = this._listeners.concat(callback)
       }
